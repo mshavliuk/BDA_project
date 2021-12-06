999006104710dadcedbcf3c7972288892c8d8417
mshavliuk <mshavliuk@users.noreply.github.com>
2021-12-06 15:35:50 +0200

    reuse generalised diagnostics functions for both models

--- logistic_regression.py
+++ logistic_regression.py
@@ -7,8 +7,8 @@ import stan
 from IPython.core.display import display
 from sklearn.preprocessing import StandardScaler
 
-from utils import suppress_stdout_stderr
 from diagnostics import psis_loo_summary, k_fold_cv
+from utils import suppress_stdout_stderr
 
 
 def build(samples: pd.DataFrame, outcomes: pd.DataFrame, verbose=False) -> stan.model.Model:
@@ -22,16 +22,18 @@ def build(samples: pd.DataFrame, outcomes: pd.DataFrame, verbose=False) -> stan.
         return stan.build(stan_code, data=stan_data, random_seed=0)
 
 
-def build_for_accuracy_check(samples, outcomes, test_samples, verbose=False) -> stan.model.Model:
+def build_for_accuracy_check(samples: pd.DataFrame, outcomes: pd.DataFrame,
+                             test_samples: pd.DataFrame, verbose=False,
+                             **kwargs) -> stan.model.Model:
     with open('logistic_regression_accuracy.stan', 'r') as file:
         stan_code = file.read()
 
     stan_data = {'N_train': len(outcomes),
                  'N_test': len(test_samples),
                  'J': samples.shape[1],
-                 'X_train': samples,
-                 'y_train': outcomes,
-                 'X_test': test_samples}
+                 'X_train': samples.values,
+                 'y_train': outcomes.values,
+                 'X_test': test_samples.values}
 
     context = contextlib.nullcontext if verbose else suppress_stdout_stderr
     with context():
