61c7c7ef45bf2ec978aeb85c92ca4d36c56a21e9
mshavliuk <mshavliuk@users.noreply.github.com>
2021-12-06 14:21:42 +0200

    refactoring: remove code duplications, remove debug output, fix formatting

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,95 +1,52 @@
 import contextlib
-import time
-from datetime import datetime
-from sklearn.preprocessing import StandardScaler
 
 import arviz as az
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import stan
-from IPython.display import display
-from stan.fit import Fit
-from sklearn.model_selection import KFold
+from IPython.core.display import display
+from sklearn.preprocessing import StandardScaler
 
-def build(N_train, N_test, features_number, X_train, y_train, X_test, verbose=False) -> stan.model.Model:
+from utils import suppress_stdout_stderr
+from diagnostics import psis_loo_summary, k_fold_cv
+
+
+def build(samples: pd.DataFrame, outcomes: pd.DataFrame, verbose=False) -> stan.model.Model:
+    with open('logistic_regression.stan', 'r') as file:
+        stan_code = file.read()
+
+    stan_data = {'N': samples.shape[0], 'J': samples.shape[1],
+                 'X': samples.values, 'y': outcomes.values}
+    context = contextlib.nullcontext if verbose else suppress_stdout_stderr
+    with context():
+        return stan.build(stan_code, data=stan_data, random_seed=0)
+
+
+def build_for_accuracy_check(samples, outcomes, test_samples, verbose=False) -> stan.model.Model:
     with open('logistic_regression_accuracy.stan', 'r') as file:
         stan_code = file.read()
 
-    stan_data = {'N_train': N_train, 
-                 'N_test': N_test, 
-                 'J': features_number,
-                 'X_train': X_train, 
-                 'y_train': y_train,
-                 'X_test': X_test}
+    stan_data = {'N_train': len(outcomes),
+                 'N_test': len(test_samples),
+                 'J': samples.shape[1],
+                 'X_train': samples,
+                 'y_train': outcomes,
+                 'X_test': test_samples}
 
-    return stan.build(stan_code, data=stan_data, random_seed=0)
+    context = contextlib.nullcontext if verbose else suppress_stdout_stderr
+    with context():
+        return stan.build(stan_code, data=stan_data, random_seed=0)
 
 
 def sample(model, verbose=False):
-    return model.sample(num_chains=4, num_samples=500, num_warmup=200)
-
-
-def jitter(array):
-    return array + np.random.rand(*array.shape)
-
-
-def psis_loo_summary(fit: Fit):
-    loo = az.loo(fit, pointwise=True)
-    display(loo)
-    fig, ax = plt.subplots(figsize=(8, 3))
-    az.plot_khat(loo, show_bins=True, ax=ax)
-    ax.set_title('Loo Linear model')
-    print(f"Mean Pareto K: {loo['pareto_k'].values.mean():.2f}")
-    plt.show()
+    context = contextlib.nullcontext if verbose else suppress_stdout_stderr
+    with context():
+        return model.sample(num_chains=4, num_samples=500, num_warmup=200)
 
 
 def get_disease_prob(fit):
     return fit['y_prob_pred'].mean(axis=1)
 
-def cross_validation(samples: pd.DataFrame, outcomes: pd.DataFrame):
-    X = samples.values
-    no_of_features = samples.shape[1]
-    y = outcomes.values
-    kf = KFold(n_splits = 5, shuffle = True)    
-    false_pos, false_neg, predicted = 0, 0, 0
-    start_time = time.time()
-    tot_test_len = 0
-
-    for train_idx, test_idx in kf.split(X):
-       # print("TRAIN: ", train_idx, "TEST: ", test_idx)
-        X_train, X_test = X[train_idx], X[test_idx]
-        y_train, y_test = y[train_idx], y[test_idx]
-
-        N_train = len(y_train)
-        N_test = len(y_test)
-        
-        model = build(N_train, N_test, no_of_features, X_train, y_train, X_test)
-        fit = model.sample(num_chains=4, num_samples=200, num_warmup=200)
-
-        prob = get_disease_prob(fit)
-        tot_test_len += len(prob)
-        print(N_test)
-        print(prob.shape)
-        print(y_test.shape)
-        for i in range(len(prob)):
-            false_pos += not y_test[i] and (prob[i] >= .5)
-            false_neg += y_test[i] and (prob[i] < .5)
-            predicted += y_test[i] == (prob[i] >= .5)
-        
-        print("---------------------------------------------------------------------------")
-        psis_loo_summary(fit)
-        summary = az.summary(fit, round_to=3, hdi_prob=0.9, var_names=['alpha', 'beta'])
-        display(summary)
-        print("---------------------------------------------------------------------------")
-
-    print(f"LOO score: {predicted / (tot_test_len) * 100:3.2f}%", end=' | ')
-    print(f"Predicted: {predicted:3d} / {tot_test_len:3d}", end=' | ')
-    print(f"False positives: {false_pos} | False negatives: {false_neg}")
-
-    end_time = time.time()
-    print(f"Total time: {end_time - start_time}")
-    return predicted
 
 def main():
     plt.rcParams['figure.dpi'] = 200
@@ -97,15 +54,15 @@ def main():
     SS = StandardScaler()
     col_to_scale = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
     data[col_to_scale] = SS.fit_transform(data[col_to_scale])
-    
+
     samples = data[['cp', 'trestbps', 'thalach', 'ca', 'oldpeak']]
     outcomes = data['target']
-    #model = build(samples, outcomes)
-    #fit = sample(model)
-    cross_validation(samples, outcomes)
-    #psis_loo_summary(fit)
-    #summary = az.summary(fit, round_to=3, hdi_prob=0.9, var_names=['alpha', 'beta'])
-    #display(summary)
+    model = build(samples, outcomes, True)
+    fit = sample(model)
+    k_fold_cv(samples, outcomes, 50)
+    psis_loo_summary(fit, 'Logistic')
+    summary = az.summary(fit, round_to=3, hdi_prob=0.9, var_names=['alpha', 'beta'])
+    display(summary)
 
 
 if __name__ == '__main__':
